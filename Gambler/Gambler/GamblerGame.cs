086731822cb3a20782e7afb3e529312a53d5d1cc
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-10 15:04:58 +0530

    [Indira] ADDED: Check till loss is 50% or win is 50%

--- Gambler/Gambler/GamblerGame.cs
+++ Gambler/Gambler/GamblerGame.cs
@@ -7,14 +7,30 @@ namespace Gambler
         public const int Stake = 100;
         public const int Bet = 1;
         public const int IS_WIN = 1;
+        
         public void GameBet()
         {
-            Random random = new Random();
-            int bets = random.Next(0, 2);
-            if (bets == IS_WIN)
-                Console.WriteLine("Win");
-            else
-                Console.WriteLine("loss")
+            int win = 0;
+            int loss = 0;
+            int cash = Stake;
+            
+            while (cash >= 50 && cash <= 150)
+            {
+                Random random = new Random();
+                int bets = random.Next(0, 2);
+                if (bets == 0)
+                {
+                    cash += 1;
+                    win += 1;
+                }
+                else
+                {
+                    cash -= 1;
+                    loss += 1;
+                }
+            }
+            Console.WriteLine("No of wins =" + win);
+            Console.WriteLine("No of Loss =" + loss);
         }   
     }
 }
