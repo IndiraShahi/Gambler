df4542e468b0e28cf279b6dd34eba72f0c78cff7
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-10 14:37:37 +0530

    [Indira] ADDED: Bet of 1 win or lose

--- Gambler/Gambler/GamblerGame.cs
+++ Gambler/Gambler/GamblerGame.cs
@@ -1,7 +1,20 @@
-﻿namespace Gambler
+﻿using System;
+
+namespace Gambler
 {
-    public class GamblerGame
+    class GamblerGame
     {
-        
+        public const int Stake = 100;
+        public const int Bet = 1;
+        public const int IS_WIN = 1;
+        public void GameBet()
+        {
+            Random random = new Random();
+            int bets = random.Next(0, 2);
+            if (bets == IS_WIN)
+                Console.WriteLine("Win");
+            else
+                Console.WriteLine("loss")
+        }   
     }
 }
--- Gambler/Gambler/Program.cs
+++ Gambler/Gambler/Program.cs
@@ -9,20 +9,8 @@ namespace Gambler
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to gambling problem ");
-            int stake = 100;
-            int bet = 1;
-            int betWin = 0;
-            Random random = new Random();
-            int bets = random.Next(0, 2);
-
-            if (bets == 0)
-            {
-                betWin = 100 + 1;
-            }
-            else
-            {
-                betWin = 100 - 1;
-            }
+            GamblerGame game = new GamblerGame();
+            game.GameBet();
         }
     }
 }
